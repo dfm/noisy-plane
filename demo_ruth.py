2c576ef8ff623d9a30767e4273f172b916e217b9
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-01-03 18:55:18 +0000

    adding real data

--- demo_ruth.py
+++ demo_ruth.py
@@ -4,17 +4,65 @@ import numpy as np
 import matplotlib.pyplot as pl
 from scipy.misc import logsumexp
 
-# def model(m, x, y):
-#     n, a, b, c = m
-#     return n * x + np.log10(a) + b*np.log10(y - c)
-
 def model(m, x, y):
-    return m[0]+m[1]*x+m[2]*y
+    return m[0] * x + np.log10(m[1]) + m[2]*np.log10(y - m[3])
+
+def log_errorbar(y, errp, errm):
+    plus = y + errp
+    minus = y - errm
+    log_err = np.log10(plus/minus) / 2.
+    l = minus < 0 # Make sure ages don't go below zero!
+    log_err[l] = np.log10(plus[l]/y[l])
+    return log_err
+
+# Load data
+data = np.genfromtxt('/Users/angusr/Python/Gyro/data/matched_data.txt').T
+mass = data[6]
+logg = data[9]
+period = data[1]
+
+# Remove subgiants, Kraft break stars and stars with period < 1
+a = (period > 1.) * (logg > 4.) * (mass < 1.3)
+
+# Assign variable names
+x = data[1][a]
+xerrp = data[2][a]
+xerrm = data[2][a]
+y = data[3][a]*1000 # Convert to Myr
+yerrp = data[4][a]*1000
+yerrm = data[5][a]*1000
+
+# Fake data
+x = np.random.uniform(2,60,len(y)) # Fake x data
+z = np.random.uniform(0.4,1.2,len(y))
+zerr = np.ones_like(z) * 0.05
+l = z < 0.4
+
+# Resample those points that are less than 0.4
+while l.sum() > 0:
+    z[l] = np.random.uniform(0.4,1.2,l.sum())
+    zerr[l] = np.ones_like(z[l]) * 0.005
+    l = z < 0.4
+
+m_true = [0.5189,  0.7725, 0.601, 0.4]
+y = model(m_true, np.log10(x), z) #+ np.random.randn(len(age)) # Fake y data
+
+# Take logs
+x = np.log10(x)
+y = np.log10(y)
+
+# Calculate logarithmic errorbars
+xerr = log_errorbar(x, xerrp, xerrm)
+yerr = log_errorbar(y, yerrp, yerrm)
+
+# Make up uncertainties for now.
+N = len(x)
+xerr = 0.01+0.01*np.random.rand(N)
+yerr = 0.01+0.01*np.random.rand(N)
+zerr = 0.01+0.01*np.random.rand(N)
 
 # Generate true values.
 N = 50
-m_true = [0.5, 0.6, 0.1]
-# m_true = [0.5189,  0.7725, 0.601, 0.4]
 x = 1 + 4*np.random.rand(N)
 y = 10 + 40*np.random.rand(N)
 z = model(m_true, x, y)
@@ -38,6 +86,7 @@ def lnlike(m):
     z_pred = model(m, x_samp, y_samp)
     chi2 = -0.5*((z_obs[:, None] - z_pred)/z_err[:, None])**2
     return np.sum(np.logaddexp.reduce(chi2, axis=1))
+#     return np.sum(logsumexp(chi2, axis=1))
 
 def lnprior(m):
     if np.any(m<0.)==False and np.any(1.<m)==False:
@@ -61,10 +110,8 @@ print("Production run")
 sampler.run_mcmc(p0, 500)
 
 print("Making triangle plot")
-# fig = triangle.corner(sampler.flatchain, truths=m_true,
-#                       labels=["$n$", "$a$", "$b$", "$c$"])
 fig = triangle.corner(sampler.flatchain, truths=m_true,
-                      labels=["$m_0$", "$m_1$", "$m_2$"])
+                      labels=["$n$", "$a$", "$b$", "$c$"])
 fig.savefig("triangle.png")
 
 print("Plotting traces")
@@ -73,6 +120,16 @@ for i in range(ndim):
     pl.clf()
     pl.plot(sampler.chain[:, :, i].T)
     pl.savefig("{0}.png".format(i))
+
+pl.figure()
+labels = ['n', 'a', 'b', 'c']
+for j in range(len(m_true)):
+    pl.subplot(len(m_true),1,j)
+    [pl.plot(sampler.chain[i, :, j], 'k-', \
+        alpha = 0.2) for i in range(nwalkers)]
+    pl.axhline(m_true[j], color = 'r')
+    pl.ylabel('%s' %labels[j])
+pl.savefig('traces')
 
 # Flatten chain
 samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
