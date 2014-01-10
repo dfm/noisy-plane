ca034475f5363eecf9fea2f5bc2bd205616a6e45
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-01-10 18:06:34 +0000

    fixing axis = 1

--- demo.py
+++ demo.py
@@ -29,7 +29,7 @@ y_samp = np.vstack([y0+ye*np.random.randn(K) for y0, ye in zip(y_obs, y_err)])
 def lnlike(m):
     z_pred = m[0]+m[1]*x_samp+m[2]*y_samp
     chi2 = -0.5*((z_obs[:, None] - z_pred)/z_err[:, None])**2
-    return np.sum(logsumexp(chi2))
+    return np.sum(np.logaddexp.reduce(chi2, axis = 1))
 
 def lnprob(m):
     return lnlike(m)
@@ -62,6 +62,6 @@ samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
 # Find values
 mcmc_result = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
                              zip(*np.percentile(samples, [16, 50, 84],
-                                                axis=0)))
+					axis=0)))
 print 'true values', m_true
-print 'mcmc result' = np.array(mcmc_result)[:,0]
+print 'mcmc result', np.array(mcmc_result)[:,0]
