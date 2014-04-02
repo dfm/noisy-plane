b4f139f646a971d0751084f4467493033bfcf571
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-04-02 15:06:14 +0100

    adding if name, etc

--- mixture.py
+++ mixture.py
@@ -11,29 +11,6 @@ import scipy.optimize as op
 import plotting
 import models
 
-# m_true = [1.927, 0.216, 0.1156, 6500.] # latest version
-m_true = [1.927, 0.216, 0.1156] # fixed T_k
-
-# generating fake data
-# x_obs, y_obs, z_obs, x_err, y_err, z_err = plotting.fake_data(m_true, 144)
-# plotting.plt(x_obs, y_obs, z_obs, x_err, y_err, z_err, m_true, "fakedata")
-
-# loading real data
-x_obs, y_obs, z_obs, x_err, y_err, z_err = plotting.load()
-# plotting.plt(x_obs, y_obs, z_obs, x_err, y_err, z_err, m_true, "realdata")
-
-print '3d plot'
-# 3d plot
-# a = y_obs < m_true[3]
-# plotting.plot2d(x_obs[a], y_obs[a], z_obs[a], x_obs[a], y_obs[a], z_obs[a], m_true, 1, 'k', "3dorig")
-plotting.plot3d(x_obs, y_obs, z_obs, x_obs, y_obs, z_obs, m_true, 1, 'k', "3dorig")
-raw_input('enter')
-
-print "Draw posterior samples."
-K = 500
-x_samp = np.vstack([x0+xe*np.random.randn(K) for x0, xe in zip(x_obs, x_err)])
-y_samp = np.vstack([y0+ye*np.random.randn(K) for y0, ye in zip(y_obs, y_err)])
-
 # Suzanne's lhf
 def lnlike(par):
     TEMP_MAX = 7500
@@ -90,42 +67,66 @@ def lnprob(m):
 # # plotting.plt(x_obs, y_obs, z_obs, x_err, y_err, z_err, result, "ml_result")
 # # plotting.plot3d(x_obs, y_obs, z_obs, x_obs, y_obs, z_obs, result, 2, 'b', "3dml")
 
-# Sample the posterior probability for m.
-nwalkers, ndim = 32, len(m_true)
-p0 = [m_true+1e-4*np.random.rand(ndim) for i in range(nwalkers)]
-# sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args = args)
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob)
-print("Burn-in")
-p0, lp, state = sampler.run_mcmc(p0, 100)
-sampler.reset()
-print("Production run")
-sampler.run_mcmc(p0, 500)
+if __name__ == "__main__":
+    # m_true = [1.927, 0.216, 0.1156, 6500.] # latest version
+    m_true = [1.927, 0.216, 0.1156] # fixed T_k
 
-print("Making triangle plot")
-fig_labels = ["$n$", "$a$", "$b$", "$T_K$", "$Z$", "$V$"]
-fig = triangle.corner(sampler.flatchain, truths=m_true, labels=fig_labels[:len(m_true)])
-fig.savefig("triangle.png")
+    # generating fake data
+    # x_obs, y_obs, z_obs, x_err, y_err, z_err = plotting.fake_data(m_true, 144)
+    # plotting.plt(x_obs, y_obs, z_obs, x_err, y_err, z_err, m_true, "fakedata")
 
-print("Plotting traces")
-pl.figure()
-for i in range(ndim):
-    pl.clf()
-    pl.axhline(m_true[i], color = "r")
-    pl.plot(sampler.chain[:, :, i].T, 'k-', alpha=0.3)
-    pl.savefig("{0}.png".format(i))
+    # loading real data
+    x_obs, y_obs, z_obs, x_err, y_err, z_err = plotting.load()
+    # plotting.plt(x_obs, y_obs, z_obs, x_err, y_err, z_err, m_true, "realdata")
 
-# Flatten chain
-samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
+    print '3d plot'
+    # 3d plot
+    # a = y_obs < m_true[3]
+    # plotting.plot2d(x_obs[a], y_obs[a], z_obs[a], x_obs[a], y_obs[a], z_obs[a], m_true, 1, 'k', "3dorig")
+    plotting.plot3d(x_obs, y_obs, z_obs, x_obs, y_obs, z_obs, m_true, 1, 'k', "3dorig")
+    raw_input('enter')
 
-# Find values
-mcmc_result = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
-                  zip(*np.percentile(samples, [16, 50, 84], axis=0)))
+    print "Draw posterior samples."
+    K = 500
+    x_samp = np.vstack([x0+xe*np.random.randn(K) for x0, xe in zip(x_obs, x_err)])
+    y_samp = np.vstack([y0+ye*np.random.randn(K) for y0, ye in zip(y_obs, y_err)])
 
-print 'initial values', m_true
-mcmc_result = np.array(mcmc_result)[:, 0]
-# print "ml result = ", result
-print 'mcmc result', mcmc_result
+    # Sample the posterior probability for m.
+    nwalkers, ndim = 32, len(m_true)
+    p0 = [m_true+1e-4*np.random.rand(ndim) for i in range(nwalkers)]
+    # sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args = args)
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob)
+    print("Burn-in")
+    p0, lp, state = sampler.run_mcmc(p0, 100)
+    sampler.reset()
+    print("Production run")
+    sampler.run_mcmc(p0, 500)
 
-# plotting result
-# plotting.plt(x_obs, y_obs, z_obs, x_err, y_err, z_err, mcmc_result, "mcmc_result")
-# plotting.plot3d(x_obs, y_obs, z_obs, x_obs, y_obs, z_obs, mcmc_result, 3, 'r', "3dmcmc")
+    print("Making triangle plot")
+    fig_labels = ["$n$", "$a$", "$b$", "$T_K$", "$Z$", "$V$"]
+    fig = triangle.corner(sampler.flatchain, truths=m_true, labels=fig_labels[:len(m_true)])
+    fig.savefig("triangle.png")
+
+    print("Plotting traces")
+    pl.figure()
+    for i in range(ndim):
+        pl.clf()
+        pl.axhline(m_true[i], color = "r")
+        pl.plot(sampler.chain[:, :, i].T, 'k-', alpha=0.3)
+        pl.savefig("{0}.png".format(i))
+
+    # Flatten chain
+    samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
+
+    # Find values
+    mcmc_result = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
+                      zip(*np.percentile(samples, [16, 50, 84], axis=0)))
+
+    print 'initial values', m_true
+    mcmc_result = np.array(mcmc_result)[:, 0]
+    # print "ml result = ", result
+    print 'mcmc result', mcmc_result
+
+    # plotting result
+    # plotting.plt(x_obs, y_obs, z_obs, x_err, y_err, z_err, mcmc_result, "mcmc_result")
+    # plotting.plot3d(x_obs, y_obs, z_obs, x_obs, y_obs, z_obs, mcmc_result, 3, 'r', "3dmcmc")
