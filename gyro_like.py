a305195868d2c43967956bb7a2784b2d033af842
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-07-15 09:42:27 +0200

    trying to fix NaN problem

--- gyro_like.py
+++ gyro_like.py
@@ -29,9 +29,13 @@ def lnlike(par, age_samp, temp_samp, period_samp, logg_samp, age_obs, age_err, \
             like12 = \
                 np.exp(-.5*((period_obs[i] - X)/(U + period_err[i]))**2) \
                 / (U + period_err[i]) # FIXME here U is sig, not var
+            if like12 > 1e3:
+                print like12, period_obs[i], period_err[i]
+            if like12 ==0:
+                print like12, period_obs[i], period_err[i], X, U
             like1 = (1-P)*like11 + P*like12
-            loglike1 = np.log((1-P)*like11 + P*like12)
-            loglik1 = np.logaddexp.reduce(loglike1, axis=0)/float(l1.sum())
+#             loglike1 = np.log((1-P)*like11 + P*like12)
+#             loglik1 = np.logaddexp.reduce(loglike1, axis=0)/float(l1.sum())
             lik1 = np.sum(like1) / float(l1.sum())
         else:
             lik1 = 0.0
@@ -39,10 +43,10 @@ def lnlike(par, age_samp, temp_samp, period_samp, logg_samp, age_obs, age_err, \
         # hot MS stars
         l2 = (temp_samp[i,:] < c) * (logg_samp[i,:] > logg_cut)
         if l2.sum() > 0:
-            like2 = np.exp(-.5*((period_obs[i] - Y)/(period_err[i]+V)**2)) \
+            like2 = np.exp(-.5*((period_obs[i] - Y)/(period_err[i]+V))**2) \
                 / (V + period_err[i]) # FIXME now V is sig, not var
-            loglike2 = np.log(like2)
-            loglik2 = np.logaddexp.reduce(loglike2, axis=0) # don't divide by J!
+#             loglike2 = np.log(like2)
+#             loglik2 = np.logaddexp.reduce(loglike2, axis=0) # don't divide by J!
             lik2 = np.sum(like2) / float(l2.sum())
         else:
             lik2 = 0.0
@@ -50,12 +54,16 @@ def lnlike(par, age_samp, temp_samp, period_samp, logg_samp, age_obs, age_err, \
         # subgiants
         l3 = (logg_samp[i,:] < logg_cut)
         if l3.sum() > 0:
-            like3 = np.exp(-.5*((period_obs[i] - Z)/(period_err[i]+U)**2)) \
+            like3 = np.exp(-.5*((period_obs[i] - Z)/(period_err[i]+U))**2) \
                 / (W + period_err[i]) # FIXME now W is sig, not var
-            loglike3 = np.log(like3)
-            loglik3 = np.logaddexp.reduce(loglike3, axis=0) # don't divide by J!
+#             loglike3 = np.log(like3)
+#             loglik3 = np.logaddexp.reduce(loglike3, axis=0) # don't divide by J!
             lik3 = np.sum(like3) / float(l3.sum())
         else:
             lik3 = 0.0
         ll[i] = np.log10(lik1 + lik2 + lik3)
-    return np.sum(ll)
+    loglike = np.sum(ll)
+    if np.isnan(loglike) == True:
+        loglike = -np.inf
+        print lik1, lik2, lik3, like12
+    return loglike
