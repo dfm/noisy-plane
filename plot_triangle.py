28c5c093f3b9026872109ab34e560a2cf3106edd
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-07-17 13:42:55 +0200

    first commit

---
+++ plot_triangle.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as pl
+import triangle
+import h5py
+
+# fname = "small_changes"
+fname = "garcia"
+
+with h5py.File("samples_%s" %fname, "r") as f:
+    samples = f["samples"][:, 50:, :]
+nwalkers, n, ndim = samples.shape
+flatchain = samples.reshape((-1, ndim))
+
+mcmc_result = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
+                  zip(*np.percentile(flatchain, [16, 50, 84], axis=0)))
+mres = np.array(mcmc_result)[:, 0]
+print 'mcmc_result = ', mres
+
+fig_labels = ["$a$", "$n$", "$b$", "$Y$", "$V$", "$Z$", "$W$", "$X$", "$U$", "$P$"]
+fig = triangle.corner(flatchain, truths=mres, labels=fig_labels)
+fig.savefig("triangle%s.png" %fname)
