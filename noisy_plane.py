725312a789dcd4579c510ad4f91b7497ee5ff007
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-06-02 09:49:02 -0400

    fixing log sampling

--- noisy_plane.py
+++ noisy_plane.py
@@ -13,13 +13,38 @@ def generate_samples(obs, u, N):
     for i in range(ndims):
         samples[i, :, :] = np.vstack([x0+xe*np.random.randn(N) for x0, xe in \
                 zip(obs[i, :], u[i, :])])
-#     plt.clf()
-#     x, y = obs[0, :], obs[1, :]
-#     xerr, yerr = u[0, :], u[1, :]
-#     plt.plot(samples[0, :, :], samples[1, :, :], "r.", markersize=2, alpha=.3)
-#     plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt="k.", capsize=0, ecolor=".7")
-#     plt.show()
-#     assert 0
+    return samples
+
+def generate_samples_log(obs, up, um, N):
+    '''
+    obs is a 2darray of observations. shape = (ndims, nobs)
+    up is a 2darray of the upper uncertainties associated with the
+    observations, um is a 2d array of the lower uncertainties.
+    returns a 3d array of samples. shape = (nobs, ndims, nsamples)
+    '''
+    plt.clf()
+    plt.errorbar(obs[0, :], obs[1], xerr=(up[0, :], um[0, :]),
+                yerr=(up[1, :], um[1, :]), fmt="k.")
+    plt.show()
+    ndims, nobs = np.shape(obs)
+    samples = np.zeros((ndims, nobs, N))
+    for j in range(nobs):
+        for i in range(ndims):
+            sp = obs[i, j] + up[i, j]*np.random.randn(2*N*int(up[i, j]/um[i, j]))
+            sm = obs[i, j] + um[i, j]*np.random.randn(2*N)
+            sp = sp[sp > obs[i, j]]
+            sm = sm[sm < obs[i, j]]
+            s = np.concatenate((sp, sm))
+            samples[i, j, :] = np.random.choice(s, N)
+
+            if i == 1:
+                plt.clf()
+#                 plt.hist(samples[i, j, :], 50)
+                plt.plot(samples[0, j, :], samples[1, j, :], "r.", alpha=.01)
+                plt.errorbar([obs[0, j]], [obs[1, j]],
+                             xerr=([um[0, j]], [up[0, j]]),
+                             yerr=([um[1, j]], [up[1, j]]), fmt="k.")
+                plt.show()
     return samples
 
 # n-D non-hierarchical log-likelihood
@@ -87,6 +112,8 @@ def lnlikeHM(pars, samples, obs, u):
         ll2[i, :] = -.5*((zobs[i] - Y)**2*inv_sigma22) + np.log(inv_sigma22)
         ll[i, :] = np.logaddexp(np.log(1-P) + ll1[i, :], np.log(P) + ll2[i, :])
     loglike = np.sum(np.logaddexp.reduce(ll, axis=1))
+    if np.isnan(loglike):
+        return -np.inf
     return loglike
 
 # 1d, hierarchical log-Likelihood
@@ -97,7 +124,9 @@ def orig_lnlike(theta, x, y, yerr):
     return -0.5*(np.sum((y-model)**2*inv_sigma2 - np.log(inv_sigma2)))
 
 if __name__ == "__main__":
+
     pars = [.5, 10]
+
     # make fake data
     nobs = 20
     x = np.random.uniform(0, 10, nobs)
@@ -110,6 +139,9 @@ if __name__ == "__main__":
     obs = np.vstack((y, x))  # dependent variable 1st
     u = np.vstack((yerr, xerr))
     nsamp = 50
+    samples = generate_samples_log(obs, 5*u, u, 5000)
+    assert 0
+
     samples = generate_samples(obs, u, nsamp)
     print lnlike(pars, samples, obs, u)
 
